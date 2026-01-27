cc09160b1e6466182f43b5b2397a95e0630102e9
Mitchel <121130718+Ezanomitch@users.noreply.github.com>
2026-01-27 00:16:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
+        hearts: ['❤️', '💖', '🦸🏾‍♀️', '💗', '💓'],  // Heart emojis
         Extras: ['🧸', '🇬🇭', '🦸🏾‍♀️']                       // Cute bear emojis
     },
 
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/ddekxmsia/video/upload/v1769490341/Future_Metro_Boomin_The_Weeknd_-_All_to_Myself_Official_Audio_hmyxbj.mp3", // Music streaming URL
+        musicUrl: "https://collection.cloudinary.com/ddekxmsia/bf69647fa940d06dbf4ad597673a998a", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
